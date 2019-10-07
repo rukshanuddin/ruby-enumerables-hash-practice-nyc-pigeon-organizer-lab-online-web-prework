4b0ff50619fcf9be8729b3cdb83bfef14ab69fe3
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-07 19:06:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,12 @@
 
 def nyc_pigeon_organizer(data)
-data.each_with_object({}) do |(key, h), result|
+data.each_with_object({}) { |(key, h), result|
   h.each do |value, names|
     names.each do |name|
       result[name] ||= {}
       result[name][key] ||= []
       result[name][key] << value.to_s
-    end
+    }
   end
 end
 end
